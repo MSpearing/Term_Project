8774dbe14980023cba3ed504324fee043fe144ff
bmwatson214 <bailey.watson214@utexas.edu>
2017-04-21 12:09:24 -0500

    Create Message.java

---
+++ src/Message.java
@@ -0,0 +1,16 @@
+
+public class Message {
+	
+	String content;
+	String author;
+	ChatRoom room;
+	
+	
+	public Message(String c, String a, ChatRoom cr){
+		this.content=c;
+		this.author=a;
+		this.room=cr;
+	}
+	
+
+}
